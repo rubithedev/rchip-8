3612033bf0de03da8d148eda71272b67f48fddac
Gabe <gabrielgrechuk@outlook.com>
2026-07-28 16:36:42 -0300

    #5 - Add Clock module to Root

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,4 @@
 pub const LoggerMod = @import("logger.zig");
 pub const RenderMod = @import("render.zig");
+pub const ClockMod = @import("chip_clock.zig");
 pub const Chip8Mod = @import("chip8.zig");
