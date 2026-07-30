61cfa663de1a89d063d3855cd40fe503536c2798
Gabe <gabrielgrechuk@outlook.com>
2026-07-30 16:42:11 -0300

    #9 - Read keyboar and implement the `0xFx0A - LD Vx, K` instruction

--- src/main.zig
+++ src/main.zig
@@ -1,37 +1,58 @@
 const std = @import("std");
 const Io = std.Io;
 
-const print = std.debug.print;
+const debug = std.debug;
 
 const rchip_8 = @import("rchip_8");
 
-const program: [30]u8 = .{
+const Chip8 = rchip_8.Chip8Mod.Chip8;
+
+const RenderMod = rchip_8.RenderMod;
+const Render = RenderMod.Render;
+const NO_REGISTER_KEYMAP = RenderMod.NO_REGISTER_KEYMAP;
+
+const Clock = rchip_8.ClockMod.ChipClock;
+
+// const program: [30]u8 = .{
+//     0xA0, 0x00, // LD I, 0x00
+//     0x60, 0x1E, // LD V0, 0x1E
+//     0x61, 0x0B, // LD V1, 0x0B
+//     0x62, 0x05, // LD V2, 0x05
+//     0x63, 0x00, // LD V3, 0x00
+//     0x64, 0x2F, // LD V4, 0x2F
+
+//     0x43, 0x10, // SEN V3, 0x10
+//     0xA0, 0x00, // LD I, 0x00
+//     0x43, 0x10, // SEN V3, 0x10
+//     0x63, 0x00, // LD V3, 0x00
+
+//     0xD0, 0x15, // DWR V0, V1, 0x5
+//     0x73, 0x01, // ADD V3, 0x01
+//     0xF2, 0x1E, // ADD I, V2
+//     0xF4, 0x15, // LD DT, V4
+//     0x12, 0x0A, // JP 0x20A
+// };
+
+const program: [14]u8 = .{
     0xA0, 0x00, // LD I, 0x00
     0x60, 0x1E, // LD V0, 0x1E
     0x61, 0x0B, // LD V1, 0x0B
-    0x62, 0x05, // LD V2, 0x05
-    0x63, 0x00, // LD V3, 0x00
-    0x64, 0x2F, // LD V4, 0x2F
 
-    0x43, 0x10, // SEN V3, 0x10
-    0xA0, 0x00, // LD I, 0x00
-    0x43, 0x10, // SEN V3, 0x10
-    0x63, 0x00, // LD V3, 0x00
-
+    0xF3, 0x0A, // Locks this shit and grab a key
+    0xF3, 0x29, // Sets key sprite to I
     0xD0, 0x15, // DWR V0, V1, 0x5
-    0x73, 0x01, // ADD V3, 0x01
-    0xF2, 0x1E, // ADD I, V2
-    0xF4, 0x15, // LD DT, V4
-    0x12, 0x0A, // JP 0x20A
+
+    0x12, 0x06, // JP 0x20A
+
 };
 
 pub fn main(init: std.process.Init) !void {
-    var render = rchip_8.RenderMod.Render.init(.{});
+    var render = Render.init(.{});
 
-    var cpu = rchip_8.Chip8Mod.Chip8.init(init.io);
+    var cpu = Chip8.init(init.io);
 
-    var cpu_clock = rchip_8.ClockMod.ChipClock.fromHz(500);
-    var timers_clock = rchip_8.ClockMod.ChipClock.fromHz(60);
+    var cpu_clock = Clock.fromHz(500);
+    var timers_clock = Clock.fromHz(60);
 
     var previous = std.Io.Clock.awake.now(init.io).toNanoseconds();
 
@@ -48,7 +69,19 @@ pub fn main(init: std.process.Init) !void {
         const cpu_ticks = cpu_clock.consume();
         const timers_ticks = timers_clock.consume();
 
-        for (0..cpu_ticks) |_| cpu.step();
+        for (0..cpu_ticks) |_| {
+            render.readKeyboard();
+            @memcpy(cpu.keyboard_buffer[0..], render.keyboard_input[0..]);
+
+            if (cpu.wait_for_key) {
+                const key = render.readKeyPressed();
+                if (key != NO_REGISTER_KEYMAP)
+                    cpu.pressedKey(@truncate(key));
+            }
+
+            cpu.step();
+        }
+
         for (0..timers_ticks) |_| cpu.tickTimers();
 
         render.draw(cpu.display_buffer);
--- src/render.zig
+++ src/render.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const print = std.debug.print;
+const debug = std.debug;
 const rlb = @import("raylib");
 
 const Color = rlb.Color;
@@ -10,6 +10,8 @@ const Types = @import("types.zig");
 const DISPLAY_WIDTH = Types.DISPLAY_WIDTH;
 const DISPLAY_HEIGHT = Types.DISPLAY_HEIGHT;
 const scale = 12;
+
+pub const NO_REGISTER_KEYMAP = 0x1F;
 
 const MonochromaticFramebuffer = Types.MonochromaticFramebuffer;
 const RGBAFrameBuffer = Types.RGBAFrameBuffer;
@@ -31,6 +33,7 @@ pub const Render = struct {
     fg_color: Color,
     texture: Texture = undefined,
     framebuffer: RGBAFrameBuffer = undefined,
+    keyboard_input: [16]u1,
     window_name: [:0]const u8,
 
     pub fn init(options: InitOptions) Render {
@@ -40,10 +43,11 @@ pub const Render = struct {
             .target_fps = options.target_fps,
             .bg_color = options.bg_color,
             .fg_color = options.fg_color,
+            .keyboard_input = @splat(0),
             .window_name = options.window_name,
         };
 
-        print("[RENDER] init(): Init Window ({}, {}, '{s}')\n", .{ self.screen_width, self.screen_height, self.window_name });
+        debug.print("[RENDER] init(): Init Window ({}, {}, '{s}')\n", .{ self.screen_width, self.screen_height, self.window_name });
         rlb.InitWindow(self.screen_width, self.screen_height, self.window_name);
         rlb.SetTargetFPS(self.target_fps);
 
@@ -55,7 +59,7 @@ pub const Render = struct {
     }
 
     pub fn deinit(self: *Render) void {
-        print("[RENDER] deinit(): Destroying render\n", .{});
+        debug.print("[RENDER] deinit(): Destroying render\n", .{});
         rlb.UnloadTexture(self.texture);
         rlb.CloseWindow();
 
@@ -65,6 +69,58 @@ pub const Render = struct {
     pub fn shouldClose(self: *Render) bool {
         _ = self;
         return rlb.WindowShouldClose();
+    }
+
+    pub fn readKeyboard(self: *Render) void {
+
+        // Skip keyboard reading if no key is pressed.
+        if (rlb.IsKeyDown(rlb.KEY_NULL)) {
+            self.keyboard_input = @splat(0);
+            return;
+        }
+
+        self.keyboard_input[0x0] = @intFromBool(rlb.IsKeyDown(rlb.KEY_ZERO));
+        self.keyboard_input[0x1] = @intFromBool(rlb.IsKeyDown(rlb.KEY_ONE));
+        self.keyboard_input[0x2] = @intFromBool(rlb.IsKeyDown(rlb.KEY_TWO));
+        self.keyboard_input[0x3] = @intFromBool(rlb.IsKeyDown(rlb.KEY_THREE));
+        self.keyboard_input[0x4] = @intFromBool(rlb.IsKeyDown(rlb.KEY_FOUR));
+        self.keyboard_input[0x5] = @intFromBool(rlb.IsKeyDown(rlb.KEY_FIVE));
+        self.keyboard_input[0x6] = @intFromBool(rlb.IsKeyDown(rlb.KEY_SIX)); //   LOL
+        self.keyboard_input[0x7] = @intFromBool(rlb.IsKeyDown(rlb.KEY_SEVEN)); // LOL
+        self.keyboard_input[0x8] = @intFromBool(rlb.IsKeyDown(rlb.KEY_EIGHT));
+        self.keyboard_input[0x9] = @intFromBool(rlb.IsKeyDown(rlb.KEY_NINE));
+        self.keyboard_input[0xA] = @intFromBool(rlb.IsKeyDown(rlb.KEY_A));
+        self.keyboard_input[0xB] = @intFromBool(rlb.IsKeyDown(rlb.KEY_B));
+        self.keyboard_input[0xC] = @intFromBool(rlb.IsKeyDown(rlb.KEY_C));
+        self.keyboard_input[0xD] = @intFromBool(rlb.IsKeyDown(rlb.KEY_D));
+        self.keyboard_input[0xE] = @intFromBool(rlb.IsKeyDown(rlb.KEY_E));
+        self.keyboard_input[0xF] = @intFromBool(rlb.IsKeyDown(rlb.KEY_F));
+    }
+
+    pub fn readKeyPressed(self: *Render) u5 {
+        _ = self;
+        const key: u5 = switch (@as(i32, @intCast(rlb.GetKeyPressed()))) {
+            rlb.KEY_ZERO => 0x0,
+            rlb.KEY_ONE => 0x1,
+            rlb.KEY_TWO => 0x2,
+            rlb.KEY_THREE => 0x3,
+            rlb.KEY_FOUR => 0x4,
+            rlb.KEY_FIVE => 0x5,
+            rlb.KEY_SIX => 0x6,
+            rlb.KEY_SEVEN => 0x7,
+            rlb.KEY_EIGHT => 0x8,
+            rlb.KEY_NINE => 0x9,
+            rlb.KEY_A => 0xA,
+            rlb.KEY_B => 0xB,
+            rlb.KEY_C => 0xC,
+            rlb.KEY_D => 0xD,
+            rlb.KEY_E => 0xE,
+            rlb.KEY_F => 0xF,
+
+            else => NO_REGISTER_KEYMAP,
+        };
+
+        return key;
     }
 
     pub fn draw(self: *Render, framebuffer: MonochromaticFramebuffer) void {
